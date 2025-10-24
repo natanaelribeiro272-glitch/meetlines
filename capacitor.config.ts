1a36ae0a1429479ba7b14b06b1a3f22fc19891fe
natanaelribeiro272-glitch <Natanael.ribeiro.272@gmail.com>
2025-10-24 17:10:53 -0300

    Finaliza merge e sincroniza projeto

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.meetlines.app',
+  appId: 'com.meetlines.meetlinesapp',
   appName: 'Meetlines',
   webDir: 'dist',
   server: {
