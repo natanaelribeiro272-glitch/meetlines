1ef1bb3df5fec9b321bc3a32d41512b28965daa9
natanaelribeiro272-glitch <natanaelribeiro272@gmail.com>
2025-11-02 05:46:37 -0300

    Updated useAndroidBackButton.tsx

--- src/hooks/useAndroidBackButton.tsx
+++ src/hooks/useAndroidBackButton.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { App as CapacitorApp } from '@capacitor/app';
 import { Capacitor } from '@capacitor/core';
 
 export const useAndroidBackButton = () => {
@@ -12,23 +11,37 @@ export const useAndroidBackButton = () => {
       return;
     }
 
-    const handleBackButton = CapacitorApp.addListener('backButton', ({ canGoBack }) => {
-      const currentPath = location.pathname;
+    let backButtonListener: any = null;
 
-      const homePaths = ['/', '/home'];
-      const isHomePage = homePaths.includes(currentPath);
+    const setupBackButton = async () => {
+      try {
+        const { App: CapacitorApp } = await import('@capacitor/app');
 
-      if (isHomePage) {
-        CapacitorApp.exitApp();
-      } else if (canGoBack) {
-        navigate(-1);
-      } else {
-        navigate('/');
+        backButtonListener = await CapacitorApp.addListener('backButton', ({ canGoBack }) => {
+          const currentPath = location.pathname;
+
+          const homePaths = ['/', '/home'];
+          const isHomePage = homePaths.includes(currentPath);
+
+          if (isHomePage) {
+            CapacitorApp.exitApp();
+          } else if (canGoBack) {
+            navigate(-1);
+          } else {
+            navigate('/');
+          }
+        });
+      } catch (error) {
+        console.log('Back button listener not available:', error);
       }
-    });
+    };
+
+    setupBackButton();
 
     return () => {
-      handleBackButton.remove();
+      if (backButtonListener) {
+        backButtonListener.remove();
+      }
     };
   }, [navigate, location]);
 };
